475429b9a80a3525f753213056230166e45b8868
Bogda <justbogda@gmail.com>
2021-01-13 23:38:27 +0200

    Adaugare clase si modificare interfata
    am adaugat codul corespunzator commituli anterior
    Signed-off-by: Bogda <justbogda@gmail.com>

--- src/main/java/ro/mta/se/lab/config.java
+++ src/main/java/ro/mta/se/lab/config.java
@@ -1,2 +1,82 @@
-package ro.mta.se.lab;public class config {
+package ro.mta.se.lab;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import ro.mta.se.lab.model.Orase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class config {
+
+    public ObservableList<String> listaorase = FXCollections.observableArrayList();
+    public ObservableList<String> listatari = FXCollections.observableArrayList();
+    Iterator it;
+    public void add_orase(String datax) {
+        String id;
+        String lat;
+        String lon;
+        String nume;
+        String tara;
+        Orase city;
+        Scanner cuvant2 = new Scanner(datax);
+        cuvant2.useDelimiter("\t");
+        String data2 = cuvant2.next();
+        id = (data2);
+        data2 = cuvant2.next();
+        nume = data2;
+        data2 = cuvant2.next();
+        lat = (data2);
+        data2 = cuvant2.next();
+        lon = (data2);
+        data2 = cuvant2.next();
+        tara = data2;
+        //System.out.println(id + "\n" + nume + "\n" + lat + "\n" + lon + "\n" + tara + "\n");
+        //listaorase.add(new Orase(nume, tara, id, lat, lon));
+        city=new Orase(nume, tara, id, lat, lon);
+        //System.out.println(city.getNume());
+        int a=0;
+
+
+            for (int i=0; i<listatari.size();i++) {
+
+                   if(listatari.get(i).equals(tara))
+                       a++;
+
+            }
+
+
+        //System.out.println(a);
+        if(a==0)
+        listatari.add(tara);
+        cuvant2.close();
+    }
+
+
+
+    public void info_orase() throws FileNotFoundException {
+
+
+        File input =new File("./src/main/resources/in.txt");
+        Scanner cuvant = new Scanner(input);
+        while (cuvant.hasNextLine()) {
+            String data = cuvant.nextLine();
+            //System.out.println(data);
+            add_orase(data);
+        }
+        cuvant.close();
+    }
+
+    public ObservableList<String> getListaorase(String tara) {
+
+        return listaorase;
+    }
+    public ObservableList<String> getListatari(){
+        return listatari;
+    }
 }
--- src/main/java/ro/mta/se/lab/controller/vremecontrol.java
+++ src/main/java/ro/mta/se/lab/controller/vremecontrol.java
@@ -1,4 +1,40 @@
 package ro.mta.se.lab.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import ro.mta.se.lab.config;
+
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class vremecontrol {
+
+    @FXML
+    private ChoiceBox choice_tara;
+
+
+
+    public void showButtonClicked(){
+        System.out.println("Arata vremea!");
+    }
+    @FXML
+    public void test_tara() throws FileNotFoundException {
+        config conf =new config();
+        conf.info_orase();
+        choice_tara.setItems(conf.getListatari());
+
+    }
+
+    public void test_oras(){
+        System.out.println("Arata orasul!");
+    }
+
+
+
+
+
 }
