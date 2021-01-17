d84e1bb1859c799cc3f480ae198757bea8f73c5c
Bogda <justbogda@gmail.com>
2021-01-17 20:20:49 +0200

    Implementare finala
    Am adaugat diagrmaele la proiect, am terminat implementarea proiectului, am modificat readme.md si alte modificari
    Signed-off-by: Bogda <justbogda@gmail.com>

--- src/main/java/ro/mta/se/lab/jsoninter.java
+++ src/main/java/ro/mta/se/lab/jsoninter.java
@@ -1,24 +1,44 @@
 package ro.mta.se.lab;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
+/**
+ * Clasa jsoninter se ocupa de parsarea json-ului primit ca parametru si de crearea unor functii prin care
+ * sunt extrase datele din acesta
+ *
+ * @author Bogdan Vlaicu
+ */
 public class jsoninter {
+
+    /**
+     * Aici sunt declarati membrii clasei, js- este de tipul string si contine json-ul primit ca parametru
+     * ceilalti membrii reprezinta datele extrase din acest json
+     */
     String js;
     double temperatura;
     double temperatura_simtita;
     double temperatura_min;
     double temperatura_max;
 
-    String rainfallMain;
+    /**
+     * Functia salveaza in membrul js json-ul returnat de functia obiectului vreme
+     *
+     * @param oras primeste numele unui oras
+     * @throws IOException
+     */
     public void getjson( String oras) throws IOException {
         infovreme vreme= new infovreme();
         js=vreme.vreme(oras);
        //System.out.println(js);
     }
-    public String parse()
+
+    /**
+     * Functia creaza un obiect json din stringul salvat in js si salveaza in ceilalti membrii informatile estrase din
+     * obiectul json.
+     */
+    public void parse()
     {
 
         JSONObject jsonObject = new JSONObject(js);
@@ -30,26 +50,41 @@ public class jsoninter {
         temperatura_max=jsonObject.getJSONObject("main").getDouble("temp_max");
         //rainfallMain = jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
 
-
+        temperatura=temperature;
         System.out.println(temperature);
         System.out.println(temperatura_max);
         System.out.println(temperatura_min);
         System.out.println(temperatura_simtita);
-        return "ana";
     }
 
+    /**
+     * Getter pentru temperatura
+      * @return temperatura actuala
+     */
     public double getTemperatura() {
-        return temperatura;
+        return (temperatura);
     }
 
+    /**
+     * Getter pentru temperatura simtita
+     * @return temperatura simtita
+     */
     public double getTemperatura_simtita() {
         return temperatura_simtita;
     }
 
+    /**
+     * Getter pentru temperatura minima
+     * @return temperatura minima
+     */
     public double getTemperatura_min() {
         return temperatura_min;
     }
 
+    /**
+     * Getter pentru temperatura maxima
+     * @return temperatura maxima
+     */
     public double getTemperatura_max() {
         return temperatura_max;
     }
